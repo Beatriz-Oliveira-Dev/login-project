ea1816b1262f4a9f4c1f35fef9866be41d40ff1b
Beatriz Oliveira <beatrizlimaoliveira1999@gmail.com>
2024-03-19 00:28:16 -0300

    Coding...> Starting business rules in kotlin

--- app/src/main/java/com/example/projectlogin/MainActivity.kt
+++ app/src/main/java/com/example/projectlogin/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.projectlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var singUP = findViewById<TextView>(R.id.singUp)
+
+        singUP.setOnClickListener{}
     }
 }
